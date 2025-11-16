e9d064b19b0dfb38ebd28ae770cc067cb6a0d257
muhammadnehal610 <muhammadnehal610@gmail.com>
2025-11-16 09:25:18 +0500

    push

--- backend/server.js
+++ backend/server.js
@@ -27,4 +27,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
